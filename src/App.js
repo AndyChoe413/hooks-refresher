3ef0c01e0d734a9aec6d9dfa64d43a021bf3f3cb
Andy Choe <Andrew.s.choe@gmail.com>
2021-10-27 10:52:42 -0400

    update1

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React, {useState,useEffect} from 'react';
 import './App.css'
 
 function App() {
-
   const [count, setCount] = useState(0)
+  const poop = <img style={{width: 100, height: 100}} src="https://elephant.art/wp-content/uploads/2019/11/poop-emoji.jpg" alt="" />
+  
 
   return (
     <div className="App">
-      <button className='buttons' onClick={() => setCount(count - 1)}>-</button>
-       <h1>{ count }</h1>
-      <button className='buttons' onClick={() => setCount(count + 1)}>+</button>
+      <div>
+        <button className='buttons' onClick={() => setCount(count - 1)}>-</button>
+        <h1>{count}</h1>
+        <button className='buttons' onClick={() => setCount(count + 1)}>+</button>
+      </div>
+      <div>
+          <h4 >{ poop }</h4>
+      </div>
     </div>
   );
 }
